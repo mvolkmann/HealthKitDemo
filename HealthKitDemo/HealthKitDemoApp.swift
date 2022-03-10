fb6c2380c1d27a41d7fb00a566a7bbc7aa16f67e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-03-10 14:57:33 -0600

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by Mark Volkmann on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
