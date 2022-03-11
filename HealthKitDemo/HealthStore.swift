5e06e9273db59d4c079b5b5ac795e9b3f0320c94
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-03-11 13:00:13 -0600

    progress

--- HealthKitDemo/HealthStore.swift
+++ HealthKitDemo/HealthStore.swift
@@ -2,7 +2,6 @@ import Foundation
 import HealthKit
 
 class HealthStore {
-    //var query: HKStatisticsCollectionQuery?
     var hkStore: HKHealthStore?
     
     init() throws {
@@ -13,9 +12,11 @@ class HealthStore {
         }
     }
     
-    func queryHeart(completion: @escaping (HKStatisticsCollection?) -> Void) {
-        let heartRate = HKQuantityType.quantityType(
-            forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+    func query(
+        type: HKQuantityType,
+        options: HKStatisticsOptions,
+        completion: @escaping (HKStatisticsCollection?) -> Void
+    ) {
         let startDate = Calendar.current.date(byAdding: .day, value: -6, to: Date())
         let anchorDate = Date.mondayAt12AM()
         let daily = DateComponents(day: 1)
@@ -25,9 +26,9 @@ class HealthStore {
             options: .strictStartDate
         )
         let query = HKStatisticsCollectionQuery(
-            quantityType: heartRate,
+            quantityType: type,
             quantitySamplePredicate: predicate,
-            options: .discreteAverage,
+            options: options,
             anchorDate: anchorDate,
             intervalComponents: daily
         )
@@ -39,30 +40,16 @@ class HealthStore {
         }
     }
     
+    func queryHeart(completion: @escaping (HKStatisticsCollection?) -> Void) {
+        let heartRate = HKQuantityType.quantityType(
+            forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+        query(type: heartRate, options: .discreteAverage, completion: completion)
+    }
+    
     func querySteps(completion: @escaping (HKStatisticsCollection?) -> Void) {
         let stepCount = HKQuantityType.quantityType(
             forIdentifier: HKQuantityTypeIdentifier.stepCount)!
-        let startDate = Calendar.current.date(byAdding: .day, value: -6, to: Date())
-        let anchorDate = Date.mondayAt12AM()
-        let daily = DateComponents(day: 1)
-        let predicate = HKQuery.predicateForSamples(
-            withStart: startDate,
-            end: Date(),
-            options: .strictStartDate
-        )
-        let query = HKStatisticsCollectionQuery(
-            quantityType: stepCount,
-            quantitySamplePredicate: predicate,
-            options: .cumulativeSum,
-            anchorDate: anchorDate,
-            intervalComponents: daily
-        )
-        query.initialResultsHandler = { query, collection, error in
-            completion(collection)
-        }
-        if let store = hkStore {
-            store.execute(query)
-        }
+        query(type: stepCount, options: .cumulativeSum, completion: completion)
     }
     
     func requestAuthorization(completion: @escaping (Bool) -> Void) {
